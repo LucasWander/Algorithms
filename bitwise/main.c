69a226de1aba32e1e9143f63571f8298714394c8
Lucas Wander de Freitas <lucaswf48@gmail.com>
2020-04-18 12:30:18 -0300

    Add bitwise examples

---
+++ bitwise/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+void and() {
+	int a = 0b10100111 & 0b00000011 == 0b00000011;
+	int b = 10100111 & 00000011;
+	printf("%d\n", a);
+	printf("%d\n", b);
+
+	if(a){
+		printf("teste\n");
+	}
+}
+
+void bitShift() {
+
+	int a = 10 << 2;
+
+	printf("%d\n",a);
+}
+
+
+int main() {
+
+	int a = 10000000 & 01111111;
+	int b = 0 ^ 0;
+
+	
+
+	bitShift();
+
+	return 0;
+}
