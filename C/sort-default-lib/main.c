4449de8c6a22b4d2e77c2d8155e6e3aa0773b00b
Lucas Wander <lucaswf48@gmail.com>
2020-11-22 10:58:48 -0300

    Add sort struct example

--- C/sort-default-lib/main.c
+++ C/sort-default-lib/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define ARRAY_SIZE 6
+
+typedef struct User{
+    char *name;
+    int age;
+}User;
 
 void printArray(int *array, int arraySize) {
 
@@ -25,15 +31,86 @@ int comparation(const void *a, const void *b) {
     return 0;
 }
 
+int comparationAge(const void *a, const void *b) {
+
+    const User *userA = (const User *) a;
+    const User *userB = (const User *) b;
+
+    int ageA = userA->age;
+    int ageB = userB->age;
+
+    if(ageA < ageB) return -1;
+    if(ageA > ageB) return 1;
+
+    return 0;
+}
+
+void sortIntArrayExample() {
+    int array[ARRAY_SIZE] = {6,4,5,2,1,3};
+    printArray(array, ARRAY_SIZE);
+    qsort(array, ARRAY_SIZE, sizeof(int), comparation);
+    printArray(array, ARRAY_SIZE);
+}
+
+User* newUsers(int size) {
+    return (User *) malloc(sizeof(User) * size);
+}
+
+User* createUser() {
+    return newUsers(1);
+}
+
+User* createUserList(int size) {
+
+    return newUsers(size);
+}
+
+int generateRandomAge() {
+    return (rand() % 110) + 1;
+}
+
+void setRandomAge(User *user) {
+    user->age = generateRandomAge();
+}
+
+void setRandomAgeList(User *users, int size) {
+
+    int count;
+
+    for(count = 0; count < size; ++count) {
+        setRandomAge(&users[count]);
+    }
+}
+
+void printfUserArray(User *users, int size) {
+    int count;
+
+    for(count = 0; count < size; ++count) {
+        printf("%d ", users[count].age);
+    }
+
+    printf("\n");
+}
+
+void sortStructArrayExample() {
+
+    int count;
+    int numberOfUsers = 50;
+
+    User *array = createUserList(numberOfUsers);
+
+    setRandomAgeList(array, numberOfUsers);
+    printfUserArray(array, numberOfUsers);
+    qsort(array, numberOfUsers, sizeof(User), comparationAge);
+    printfUserArray(array, numberOfUsers);
+}
+
 int main() {
 
-    int array[ARRAY_SIZE] = {6,4,5,2,1,3};
+    srand(time(NULL));
 
-    printArray(array, ARRAY_SIZE);
-
-    qsort(array, ARRAY_SIZE, sizeof(int), comparation);
-
-    printArray(array, ARRAY_SIZE);
+    sortIntArrayExample();
+    sortStructArrayExample();
 
     return 0;
 }
